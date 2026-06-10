5f2aa7fe441681c8546c732033a5e52407573fbb
Oleg Mihailik <mihailik@gmail.com>
2026-06-10 08:49:03 +0100

    Investigating native rusticated buffer overrun.

--- demo/loch/src/app.rs
+++ demo/loch/src/app.rs
@@ -21,13 +21,23 @@ pub struct App {
 
 impl App {
     pub async fn new() -> Self {
-        extern crate std as real_std;
-        let p = real_std::env::current_dir().unwrap_or_else(|_| real_std::path::PathBuf::from("."));
+        print!(" std::env::current_dir().unwrap_or_else()...");
+        let p = std::env::current_dir().unwrap_or_else(|_| std::path::PathBuf::from("."));
+        print!(" p.to_string_lossy().into_owned()...");
         let p_str = p.to_string_lossy().into_owned();
 
+        print!(" left_files=Self::load_dir()...");
+        let left_files = Self::load_dir(&p_str).await;
+        print!(" OK[{}]", left_files.len());
+
+        print!(" right_files=Self::load_dir()...");
+        let right_files = Self::load_dir(&p_str).await;
+        print!(" OK[{}]", right_files.len());
+
+        print!(" Self...");
         Self {
-            left_files: Self::load_dir(&p_str).await,
-            right_files: Self::load_dir(&p_str).await,
+            left_files: left_files,
+            right_files: right_files,
             left_selected: 0,
             right_selected: 0,
             current_left_dir: p_str.clone(),
@@ -75,6 +85,7 @@ impl App {
             name: String::from(".."),
             is_dir: true,
         }];
+        print!(" read_dir({})...", path);
         if let Ok(dir) = read_dir(path).await {
             for entry in dir {
                 if let Ok(entry) = entry {
@@ -86,6 +97,9 @@ impl App {
                 }
             }
         }
+        print!(" OK[{} entries]", files.len());
+
+        print!(" sorting...");
         files.sort_unstable_by(|a, b| {
             if a.name == ".." {
                 return std::cmp::Ordering::Less;
@@ -97,6 +111,7 @@ impl App {
                 .cmp(&a.is_dir)
                 .then_with(|| a.name.to_lowercase().cmp(&b.name.to_lowercase()))
         });
+        print!(" OK");
         files
     }
 }
