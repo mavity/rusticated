b1c9b9a8112d01a401dd7216676f027a385b6594
Oleg Mihailik <mihailik@gmail.com>
2026-06-21 11:09:16 +0100

    Command line parameters for kabibi-go.

--- kabibi-go/main.go
+++ kabibi-go/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
 	"os"
 
@@ -10,10 +12,87 @@ import (
 var AppProgram *tea.Program
 
 func main() {
+	helpPtr := flag.Bool("help", false, "Show help")
+	commandPtr := flag.String("c", "", "Run a single command string")
+
+	// Filter os.Args to remove elements that might be interpreted as flags but are actually metadata/setup
+	// Washmhost/Mohabbat dev-run usually passes [wasm_path -- [args...]]
+	// We want to skip everything until the first '--' OR skip known wrappers.
+	filteredArgs := []string{os.Args[0]}
+	foundSeparator := false
+	for i := 1; i < len(os.Args); i++ {
+		arg := os.Args[i]
+		if arg == "--" && !foundSeparator {
+			foundSeparator = true
+			continue
+		}
+		if !foundSeparator {
+			// Skip metadata if we haven't found the separator yet
+			if arg == "kabibi-go" || arg == "-r" {
+				continue
+			}
+		}
+		filteredArgs = append(filteredArgs, arg)
+	}
+	os.Args = filteredArgs
+
+	flag.Parse()
+
+	if *helpPtr {
+		fmt.Printf("Usage: kabibi-go [options] [script_file [args...]]\n\n")
+		fmt.Printf("Options:\n")
+		flag.PrintDefaults()
+		fmt.Printf("\nDescription:\n")
+		fmt.Printf("  kabibi-go is an AI-enhanced file manager and shell.\n")
+		fmt.Printf("  If a command string (-c) or script file is provided, it runs in batch mode.\n")
+		fmt.Printf("  Otherwise, it starts in interactive TUI mode.\n")
+		return
+	}
+
+	// Batch mode: -c "command"
+	if *commandPtr != "" {
+		runBatchCommand(*commandPtr, nil)
+		return
+	}
+
+	// Batch mode: script_file
+	if flag.NArg() > 0 {
+		runBatchFile(flag.Arg(0), flag.Args()[1:])
+		return
+	}
+
 	m := initialModel()
 	AppProgram = tea.NewProgram(&m)
 	if _, err := AppProgram.Run(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
 		os.Exit(1)
 	}
+}
+
+func runBatchCommand(cmdStr string, args []string) {
+	ctx := context.Background()
+	r, err := createRunner(ctx, os.Stdin, os.Stdout, os.Stderr, "", nil)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating runner: %v\n", err)
+		os.Exit(1)
+	}
+	r.Run(ctx, parseCommand(cmdStr))
+}
+
+func runBatchFile(filePath string, args []string) {
+	ctx := context.Background()
+	r, err := createRunner(ctx, os.Stdin, os.Stdout, os.Stderr, "", args)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error creating runner: %v\n", err)
+		os.Exit(1)
+	}
+
+	f, err := os.Open(filePath)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error opening script: %v\n", err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	r.Run(ctx, parseCommandReader(f, filePath))
 }
--- kabibi-go/shell.go
+++ kabibi-go/shell.go
@@ -3,9 +3,14 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
+	"os"
+	"path/filepath"
 	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"mvdan.cc/sh/moreinterp/coreutils"
+	"mvdan.cc/sh/v3/interp"
 	"mvdan.cc/sh/v3/syntax"
 )
 
@@ -14,6 +19,95 @@ type shellResultMsg struct {
 	input  string
 	output []string
 	err    error
+}
+
+func parseCommand(input string) *syntax.File {
+	return parseCommandReader(strings.NewReader(input), "")
+}
+
+func parseCommandReader(r io.Reader, name string) *syntax.File {
+	parser := syntax.NewParser()
+	f, err := parser.Parse(r, name)
+	if err != nil {
+		// Return empty file on error, the runner will handle it or we should
+		return &syntax.File{}
+	}
+	return f
+}
+
+func createRunner(ctx context.Context, stdin io.Reader, stdout, stderr io.Writer, dir string, args []string) (*interp.Runner, error) {
+	if dir == "" {
+		dir, _ = os.Getwd()
+	}
+
+	opts := []interp.RunnerOption{
+		interp.Dir(dir),
+		interp.StdIO(stdin, stdout, stderr),
+		interp.Params(args...),
+	}
+
+	r, err := interp.New(opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	// Custom ExecHandler for recursive script execution and u-root builtins
+	h := func(ctx context.Context, args []string) error {
+		hc := interp.HandlerCtx(ctx)
+
+		if len(args) == 0 {
+			return nil
+		}
+
+		path := args[0]
+		// 1. Check if it is a script (heuristically or by looking at it)
+		// We try to find it relative to current dir
+		absPath := path
+		if !filepath.IsAbs(path) {
+			absPath = filepath.Join(hc.Dir, path)
+		}
+
+		if info, err := os.Stat(absPath); err == nil && !info.IsDir() {
+			// Check if it's a shell script (extension or shebang)
+			isScript := strings.HasSuffix(path, ".sh")
+			if !isScript {
+				// Peek for shebang
+				f, _ := os.Open(absPath)
+				if f != nil {
+					buf := make([]byte, 2)
+					f.Read(buf)
+					f.Close()
+					if string(buf) == "#!" {
+						isScript = true
+					}
+				}
+			}
+
+			if isScript {
+				// RECURSION: Spawn a new runner for the script
+				subArgs := args[1:]
+				subRunner, err := createRunner(ctx, hc.Stdin, hc.Stdout, hc.Stderr, hc.Dir, subArgs)
+				if err != nil {
+					return err
+				}
+
+				f, err := os.Open(absPath)
+				if err != nil {
+					return err
+				}
+				defer f.Close()
+
+				return subRunner.Run(ctx, parseCommandReader(f, path))
+			}
+		}
+
+		// 2. Delegate to moreinterp/coreutils (u-root backed)
+		coreHandler := coreutils.ExecHandler(interp.DefaultExecHandler(0))
+		return coreHandler(ctx, args)
+	}
+
+	interp.ExecHandler(h)(r)
+	return r, nil
 }
 
 func (m *model) runShellCommand(input string) tea.Cmd {
@@ -30,7 +124,7 @@ func (m *model) runShellCommand(input string) tea.Cmd {
 		var sb strings.Builder
 		m.shellOut.SetTarget(&sb)
 		defer m.shellOut.SetTarget(nil)
-		
+
 		err = m.runner.Run(context.Background(), f)
 
 		res := shellResultMsg{
